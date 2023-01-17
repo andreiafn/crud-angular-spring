c3a69c5a48df4c473512f27f08d67c52de030fb0
Andreia de Freitas Novais <andreiafreitaas@hotmail.com>
2023-01-17 15:55:39 -0300

    adicionando services

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  CoursesService: CoursesService;
+
+  constructor(){
+    // this.courses = [];
+    this.CoursesService = new CoursesService();
+    this.courses = this.CoursesService.list();
+  }
+
+  ngOnInit():void {}
 
 }
